32bafe63da4a583c1d7c690233a6ecff79061e64
Shady-web <ernestsimex@gmail.com>
2024-07-24 13:54:10 +0100

    added user login and authentication using djoser library

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -5,11 +5,10 @@ from rest_framework import generics, viewsets, permissions
 from rest_framework.decorators import api_view
 from .models import MenuItem, Booking
 from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
-# Create your views here.
-#def index(request):
-#    return render(request, 'index.html', {})
+
 
 class MenuItemView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = MenuItem.objects.all()
     serializer_class= MenuItemSerializer
 
@@ -18,6 +17,7 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
     serializer_class= MenuItemSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class= BookingSerializer
 
